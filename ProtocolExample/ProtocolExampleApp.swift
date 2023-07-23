46bcb92a7cd69dbb863294d783a118811f5112b0
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-07-23 21:59:54 +0700

    Initial Commit

---
+++ ProtocolExample/ProtocolExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtocolExampleApp.swift
+//  ProtocolExample
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ProtocolExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
