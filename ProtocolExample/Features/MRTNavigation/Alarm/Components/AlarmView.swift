894a55b02fbc34c74c8f740e1ee6017e2648bc36
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-07-24 01:34:20 +0700

    feat: add alarm view

---
+++ ProtocolExample/Features/MRTNavigation/Alarm/Components/AlarmView.swift
@@ -0,0 +1,42 @@
+//
+//  Alarm.swift
+//  ProtocolExample
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 24/07/23.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    @State private var isSoundOn: Bool {
+        didSet {
+            _ = useCase.play()
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1.0, execute: {
+                _ = useCase.stop()
+            })
+        }
+    }
+    
+    private let useCase: AlarmManager
+    
+    init(isSoundOn: Bool, useCase: AlarmManager = AlarmManagerImpl()) {
+        self._isSoundOn = State(wrappedValue: isSoundOn)
+        self.useCase = useCase
+    }
+    
+    var body: some View {
+        Button {
+            isSoundOn.toggle()
+        } label: {
+            Image(isSoundOn ? "SoundOn" : "SoundOff")
+        }
+    }
+}
+
+#if DEBUG
+struct AlarmView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmView(isSoundOn: true)
+    }
+}
+#endif
